68fbc5a03f24ab97c832e41a28f021a8419fbd6c
Thamires Garcia <thamiresgarcia@Thamiress-MBP.Home>
2023-01-02 17:47:33 +0000

    feat(sorting): inicial sorting functions

---
+++ index.js
@@ -0,0 +1,7 @@
+import Sorting from "./sorting/sorting.js";
+
+const sorting = new Sorting();
+
+const array = [7, 2, 10, 3, 1];
+sorting.insertSort(array)
+console.log(array);
---
+++ sorting.js
@@ -0,0 +1,50 @@
+export default class Sorting {
+  bubble(array) {
+    for (let indexLeft = 0; indexLeft < array.length; indexLeft++) {
+      let left = array[indexLeft];
+      for (let indexRight = 0; indexRight < array.length; indexRight++) {
+        if (left < array[indexRight]) {
+          array[indexLeft] = array[indexRight];
+          array[indexRight] = left;
+          left = array[indexLeft];
+        }
+      }
+    }
+  }
+
+  selectionSort(array) {
+    for (let smallerIndex = 0; smallerIndex < array.length; smallerIndex++) {
+      let smaller = array[smallerIndex];
+      for (let index = 0; index < array.length; index++) {
+        if (array[index] > smaller) {
+          let temp = array[index];
+          array[index] = smaller;
+          smaller = temp;
+        }
+      }
+      array[smallerIndex] = smaller;
+    }
+  }
+
+  insertSort(array) {
+    for (let index = 0; index < array.length; index++) {
+      let currentyIndex = index;
+      while (array[currentyIndex] < array[currentyIndex - 1]) {
+        let temp = array[currentyIndex - 1];
+        array[currentyIndex - 1] = array[currentyIndex];
+        array[currentyIndex] = temp;
+        currentyIndex--;
+      }
+    }
+  }
+
+  mergeSort() {}
+
+  quickSort() {}
+
+  randomQuickSort() {}
+
+  countingSort() {}
+
+  radixSort() {}
+}
