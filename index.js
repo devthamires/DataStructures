39003ca134d0a18aa8667a43f5658bc139d2e881
Thamires Garcia <thamiresgarcia@Thamiress-MBP.Home>
2023-01-11 15:46:16 +0000

    feat(sorting): creating merge sort algorithm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Sorting from "../src/sorting";
+import { Sorting } from "./src/sorting.js";
 
 const sorting = new Sorting();
 
 const array = [7, 2, 10, 3, 1];
-sorting.insertSort(array)
-console.log(array);
+sorting.mergeSort(array);
+console.log(array)
--- src/sorting.js
+++ src/sorting.js
@@ -1,4 +1,4 @@
-export default class Sorting {
+export class Sorting {
   bubble(array) {
     for (let indexLeft = 0; indexLeft < array.length; indexLeft++) {
       let left = array[indexLeft];
@@ -38,10 +38,37 @@ export default class Sorting {
     }
   }
 
-  mergeSort(array) {
-    for (let index = 0; index < array.length; index++) {
-      let [elementindex] = array[index];
-      console.log(elementindex)
+  mergeSort(array, start = 0, end = null) {
+    if (end === null) end = array.length;
+
+    if (end - start > 1) {
+      let middle = Math.floor((end + start) / 2);
+      this.mergeSort(array, start, middle);
+      this.mergeSort(array, middle, end);
+      this._merge(array, start, middle, end);
+    }
+  }
+
+  _merge(array, start, middle, end) {
+    let leftArray = array.slice(start, middle);
+    let rightArray = array.slice(middle, end);
+    let topRight = 0;
+    let topLeft = 0;
+
+    for (let index = start; index < end; index++) {
+      if (topLeft >= leftArray.length) {
+        array[index] = rightArray[topRight];
+        topRight++;
+      } else if (topRight >= rightArray.length) {
+        array[index] = leftArray[topLeft];
+        topLeft++;
+      } else if (leftArray[topLeft] < rightArray[topRight]) {
+        array[index] = leftArray[topLeft];
+        topLeft++;
+      } else {
+        array[index] = rightArray[topRight];
+        topRight++;
+      }
     }
   }
 
